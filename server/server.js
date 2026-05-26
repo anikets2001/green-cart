d7443c94fcfb70ffbdaf0f6a64d39abe579df569
anket.singh <aniket.singh@yatra.com>
2026-05-26 12:48:16 +0530

    feat: mongodb connection done

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
+import connectDb from './configs/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+await connectDb();
+
+// allow multiple origins for CORS
+const allowedOrigins = [
+  'http://localhost:5173',
+]
+
+// middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res) => {
   res.send('Api is working');
